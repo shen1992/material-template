d0dc63fde46e7e35c4938d2319b9defcc22f108c
youshiyu <youshiyu@corp.netease.com>
2021-08-27 16:35:52 +0800

    feat: page模板生成

--- config/mock.js
+++ config/mock.js
@@ -1,12 +1,3 @@
 module.exports = {
-  tableSchema: [{
-    title: '用户名',
-    dataIndex: "blogName"
-  }, {
-    title: '用户ID',
-    dataIndex: 'blogId',
-  },{
-    title:"用户年龄",
-    dataIndex:'age'
-  }],
+
 };
